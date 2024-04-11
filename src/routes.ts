8f7c6c8f467689263b789de671de09d8019b7222
hrishitsarkar <hrishitjohn@gmail.com>
2024-04-11 13:04:00 +0530

    models and controllers

---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+import { Express, Request, Response } from "express";
+import { createUserHandler } from "./controller/user.controller";
+const routes = (app: Express) => {
+  app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
+
+  app.post("/api/users", createUserHandler);
+};
+
+export default routes;
